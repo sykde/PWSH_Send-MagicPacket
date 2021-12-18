d9fd7ea6bc7805da09d12749256bb59821699956
sykde <76517722+sykde@users.noreply.github.com>
2021-12-19 00:56:04 +0200

    Add files via upload
    Added the script and an example JSON file

---
+++ Send-MagicPackage.ps1
@@ -0,0 +1,56 @@
+<#
+    This script displays a list of MAC addresses from a list,
+    prompts the user to decide whether to pick one of the
+    existing addresses or provide their own input and then
+    broeadcasts a WOL package in port 0.
+#>
+
+$knownAdaptors = Get-Content -Path $env:HOMEPATH\.config\WakeOnLAN\targetsList.json -ErrorAction SilentlyContinue | ConvertFrom-Json
+
+Write-Host @"
+
+List of known hosts:
+
+"@
+
+$count = 0
+
+foreach ($adaptor in $knownAdaptors)
+{
+    Write-Host "[$count]>" $adaptor.MAC "`t Friendly Name: " $adaptor.friendlyName
+    $count += 1
+}
+
+Write-Host @"
+[$count]> User-defined
+
+"@
+
+Write-Host -NoNewline "Choose target of Magic Package: "
+[Int16] $target = Read-Host
+
+if ($targetMac -eq $count)
+{
+    Write-Host -NoNewline "MAC of target: "
+    $targetAdaptor = Read-Host
+}
+elseif (($target -gt $count) -or ($target -lt 0))
+{
+    throw "Invalid input value"
+}
+else
+{
+    [PSCustomObject] $targetAdaptor = $knownAdaptors[$target]
+}
+
+[Byte[]] $startBytes = ,0xFF * 6
+
+$targetAdaptor.MAC = $targetAdaptor.MAC.Replace(':','-')
+[Byte[]] $macBytes = ($targetAdaptor.MAC.Split('-') | ForEach-Object { [Byte] "0x$_"}) * 16
+
+[Byte[]] $magicPacketContents = $startBytes + $macBytes
+
+$UdpClient = New-Object System.Net.Sockets.UdpClient
+$UdpClient.Connect([System.Net.IPAddress]::Broadcast,0)
+$UdpClient.Send($magicPacketContents, $magicPacketContents.Length) | Out-Null #Suppress Output
+$UdpClient.Close()
